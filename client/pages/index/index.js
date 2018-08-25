811d4da4d70be22d3f6a48c26abad73a7247ef7f
CodeingBoy <CodeingBoy@gmail.com>
2018-08-25 21:43:13 +0800

    Add recommended product section

--- client/pages/index/index.js
+++ client/pages/index/index.js
@@ -5,7 +5,45 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    productList: [{
+      id: 1,
+      image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product1.jpg',
+      name: 'Item 1',
+      price: 100,
+      source: 'China-GuangDong',
+    }, {
+      id: 2,
+      image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product2.jpg',
+      name: 'Item 2',
+      price: 200,
+      source: 'China-GuangDong',
+    }, {
+      id: 3,
+      image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product3.jpg',
+      name: 'Item 3',
+      price: 300,
+      source: 'China-GuangDong',
+    }, {
+      id: 4,
+      image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product4.jpg',
+      name: 'Item 4',
+      price: 400,
+      source: 'China-GuangDong',
+    }, {
+      id: 5,
+      image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product5.jpg',
+      name: 'Item 5',
+      price: 500,
+      source: 'China-GuangDong',
+    }],
+    recommendedProduct: {
+      id: 1,
+      image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product1.jpg',
+      promotionImage: '/images/discount.png',
+      name: 'Item 1',
+      price: 100,
+      source: 'China-GuangDong',
+    }
   },
 
   /**
