c5e85aaf835b616efb276732988bdad4f992eb59
CodeingBoy <CodeingBoy@gmail.com>
2018-09-08 17:34:21 +0800

    Add loading and fail prompting

--- client/pages/index/index.js
+++ client/pages/index/index.js
@@ -15,11 +15,32 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
+    this.loadProducts();
+  },
+
+  loadProducts: function(callback){
+    wx.showLoading({
+      title: 'Loading'
+    });
+
+    const onFinishLoading = function () {
+      wx.hideLoading();
+      typeof callback === 'function' && callback();
+    };
+
+    const onFailLoadingProducts = function(){
+      wx.showToast({
+        title: 'Fail loading products',
+        icon: 'none'
+      });
+    }
+
     const page = this;
     qcloud.request({
       url: config.service.productList,
-      success: function(response) {
-        if (response.data.code !== 0) {
+      success: function (response) {
+        if (response.data.code != 0) {
+          onFailLoadingProducts();
           console.log(response);
           return;
         }
@@ -30,9 +51,14 @@ Page({
           "productList": response.data.data,
           "recommendedProduct": recommendedProduct
         });
+
+        onFinishLoading();
       },
-      fail: function(err) {
+      fail: function (err) {
         console.log(err);
+
+        onFinishLoading();
+        onFailLoadingProducts();
       }
     });
   },
