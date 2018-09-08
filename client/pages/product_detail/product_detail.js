5d5cd7624a7c9f0c0ccfeea7fca7b7f359ca4595
CodeingBoy <CodeingBoy@gmail.com>
2018-09-09 00:02:50 +0800

    Add structures and styles for product detail page

--- client/pages/product_detail/product_detail.js
+++ client/pages/product_detail/product_detail.js
@@ -5,7 +5,13 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    product: {
+      id: 1,
+      image: 'https://product-1256088332.cos.ap-guangzhou.myqcloud.com/product2.jpg',
+      name: '商品',
+      price: 480.5,
+      source: '国内·广东'
+    },
   },
 
   /**
