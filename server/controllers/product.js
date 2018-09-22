177b3d745b65e1fece5676605edb4205e5adb887
CodeingBoy <CodeingBoy@gmail.com>
2018-09-22 17:11:57 +0800

    Add product detail API

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -3,5 +3,9 @@ const db = require("../utils/db.js");
 module.exports = {
   list: async ctx => {
     ctx.state.data = await db.query("SELECT * FROM product;");
+  },
+  detail: async ctx => {
+    const productId = Number(ctx.params.id);
+    ctx.state.data = await db.query("SELECT * FROM product WHERE id = ?", [productId]);
   }
 }
