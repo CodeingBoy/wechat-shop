0384a5f14a47fba1059453212c8829a4a5af1fbd
CodeingBoy <CodeingBoy@gmail.com>
2018-09-22 17:19:22 +0800

    Make product detail API more robust

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -6,6 +6,16 @@ module.exports = {
   },
   detail: async ctx => {
     const productId = Number(ctx.params.id);
-    ctx.state.data = await db.query("SELECT * FROM product WHERE id = ?", [productId]);
+    if(isNaN(productId)){
+      ctx.state.data = {};
+      return;
+    }
+
+    const results = await db.query("SELECT * FROM product WHERE id = ?", [productId]);
+    if(results && results[0]){
+      ctx.state.data = results[0];
+    }else{
+      ctx.state.data = {};
+    }
   }
 }
