8279c7f6e898c8d232bdafc2a132dadec8bd4eea
CodeingBoy <CodeingBoy@gmail.com>
2018-09-27 23:53:33 +0800

    Comment image uploading supported

--- client/pages/comment_add/comment_add.js
+++ client/pages/comment_add/comment_add.js
@@ -38,7 +38,7 @@ Page({
     const formDetails = event.detail.value;
     const content = formDetails.content;
 
-    if(!content){
+    if (!content) {
       wx.hideLoading();
       wx.showModal({
         title: 'Comment is empty',
@@ -47,46 +47,98 @@ Page({
       return;
     }
 
-    const onComplete = function() {
-      wx.hideLoading();
-    }
+    const page = this;
 
-    qcloud.request({
-      url: config.service.addComment,
-      login: true,
-      method: 'POST',
-      data: {
-        productId: this.data.product.id,
-        content: content,
-        images: this.data.images
-      },
-      success: function(response) {
+    this.uploadImages(function(completeCount, succeedCount, uploadedImages) {
+      if (succeedCount !== completeCount) {
         wx.showToast({
-          title: 'Success',
-          icon: 'suceess'
-        });
-
-        onComplete();
-      },
-      fail: function(error) {
-        wx.showToast({
-          title: 'Submit failed, please try again later',
+          title: 'Upload image failed, please try again later',
           icon: 'none'
         });
-        console.log(error);
-
-        onComplete();
+        return;
       }
+
+      const onComplete = function() {
+        wx.hideLoading();
+      };
+
+      qcloud.request({
+        url: config.service.addComment,
+        login: true,
+        method: 'POST',
+        data: {
+          productId: page.data.product.id,
+          content: content,
+          images: uploadedImages
+        },
+        success: function(response) {
+          wx.navigateBack();
+
+          wx.showToast({
+            title: 'Success',
+            icon: 'suceess'
+          });
+
+          onComplete();
+        },
+        fail: function(error) {
+          wx.showToast({
+            title: 'Submit failed, please try again later',
+            icon: 'none'
+          });
+          console.log(error);
+
+          onComplete();
+        }
+      });
     });
   },
   onTapChooseImage: function(event) {
     const page = this;
     wx.chooseImage({
+      count: 3,
       success: function(res) {
         page.setData({
-          images: res.tempFiles
+          images: res.tempFilePaths
         });
       },
     });
+  },
+  onTapImages: function(event) {
+    const imagePath = event.currentTarget.dataset.imagePath;
+    wx.previewImage({
+      urls: this.data.images,
+      current: imagePath
+    });
+  },
+  uploadImages: function(onComplete) {
+    const images = this.data.images;
+    const imageCount = images.length;
+    var completedCount = 0,
+      succeedCount = 0;
+
+    var uploadedImages = new Array(imageCount);
+    for (let i = 0; i < imageCount; i++) {
+      const index = i;
+      wx.uploadFile({
+        url: config.service.uploadUrl,
+        filePath: images[i],
+        name: 'file',
+        success: function(response) {
+          uploadedImages[index] = JSON.parse(response.data).data.imgUrl;
+          completedCount++;
+          succeedCount++;
+          if (completedCount === imageCount) {
+            onComplete && onComplete(completedCount, succeedCount, uploadedImages);
+          }
+        },
+        fail: function(error) {
+          completedCount++;
+          if (completedCount === imageCount) {
+            onComplete && onComplete(completedCount, succeedCount);
+          }
+        }
+      });
+    }
   }
 });
--- server/controllers/comment.js
+++ server/controllers/comment.js
@@ -7,8 +7,14 @@ module.exports = {
     const avatarUrl = ctx.state.$wxInfo.userinfo.avatarUrl;
     const productId = ctx.request.body.productId;
     const content = ctx.request.body.content;
+    var images = ctx.request.body.images || [];
+    var imagesString = null;
 
-    await db.query("INSERT INTO comment(user, username, avatar, content, images, product_id, create_time) VALUES (?, ?, ?, ?, ?, ?, CURRENT_TIMESTAMP)", [userId, username, avatarUrl, content, null, productId]);
+    if (images) {
+      imagesString = images.join(";;");
+    }
+
+    await db.query("INSERT INTO comment(user, username, avatar, content, images, product_id, create_time) VALUES (?, ?, ?, ?, ?, ?, CURRENT_TIMESTAMP)", [userId, username, avatarUrl, content, imagesString, productId]);
   },
   get: async ctx => {
     const productId = Number(ctx.params.id);
